cc31661954f5c0624fdbc583e4eadae6210a59e0
Mahmoud alzoubi <mahmoudalzoubi073@gmail.com>
2021-06-29 00:33:26 +0300

    rearrange

--- linked_list/linked_list.py
+++ linked_list/linked_list.py
@@ -9,7 +9,6 @@ class Linkedlist:
 
   def appen(self,data):
     new_node = Node(data)
-    # current= self.head
     if self.head:
       new_node.next = self.head
     self.head = new_node
@@ -37,6 +36,32 @@ class Linkedlist:
       self.appen(i)
 
 
+  def rearrange(self,head):
+      elem = []
+      i =0
+      temp = self.head
+
+      while temp!=None:
+          elem.append(temp.data)
+          temp = temp.next
+      print(temp)
+      
+      if len(elem):
+          return
+
+      temp = self.head
+      while len(elem)>0:
+          if i%2==0:
+              temp.data = elem[0]
+              elem.pop(0)
+
+          else:
+              temp.data = elem[-1]
+              elem.pop()
+          i+=1
+          temp = temp.next
+
+
   def __str__(self):
     current= self.head
     list=""
@@ -51,14 +76,19 @@ if __name__=="__main__":
   L.appen("alzoubi")
   L.appen("Hasan")
   L.appen("mahmoud")
-  print(L)
-  L.reversing()
-  print(L)
-  L.deletenode("Hasan")
-  print(L)
+  # print(L)
+  # L.reversing()
+  # print(L)
+  # L.deletenode("Hasan")
+  # print(L)
 
   M = Linkedlist()
-  M.appen("1")
-  M.appen("2")
+  M.appen("6")
+  M.appen("5")
+  M.appen("4")
   M.appen("3")
+  M.appen("2")
+  M.appen("1")
+  M.rearrange(M)
   print(M)
+ 
---
+++ linked_list/rearrange.py
@@ -0,0 +1,28 @@
+def rearrange(head):
+    elem = []
+    i =0
+    temp = head
+
+    while temp!=None:
+        elem.append(temp.data)
+        temp = temp.next
+    print(temp)
+    
+    if len(elem):
+        return
+
+    temp = head
+    while len(elem)>0:
+        if i%2==0:
+            temp.data = elem[0]
+            elem.pop(0)
+
+        else:
+            temp.data = elem[-1]
+            elem.pop()
+        i+=1
+        temp = temp.next
+
+    return head
+
+
