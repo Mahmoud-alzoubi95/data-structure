525d8ba687d1ed10667578b8862fbff0276a31cd
Mahmoud alzoubi <mahmoudalzoubi073@gmail.com>
2021-07-02 22:10:30 +0300

    add

---
+++ tree/path_sum.py
@@ -0,0 +1,16 @@
+def hasPathSum(node, s):
+        ans = 0
+        subSum = s - node.data
+ 
+        # If we reach a leaf node and sum becomes 0, then
+        # return True
+        if(subSum == 0 and node.left == None and node.right == None):
+            return True
+ 
+        # Otherwise check both subtrees
+        if node.left is not None:
+            ans = ans or hasPathSum(node.left, subSum)
+        if node.right is not None:
+            ans = ans or hasPathSum(node.right, subSum)
+ 
+        return ans
