241c58ab197767f60864d8af760838a4a02e6024
Mahmoud alzoubi <mahmoudalzoubi073@gmail.com>
2021-07-03 14:56:58 +0300

    sum nodes in range

--- tree/Count_BST_nodes_in_range.py
+++ tree/Count_BST_nodes_in_range.py
@@ -25,3 +25,51 @@ def getCount(root, low, high):
  
     else:
         return getCount(root.left, low, high)
+
+
+
+# Function to perform level order traversal on the Tree and calculate the required sum
+
+# declare variable to store the sum inside it
+# Stores the nodes while performing level order traversal
+# Push the root node into the queue
+# Iterate until queue is empty
+# Stores the front node of the queue
+# If the value of the node lies in the given range
+# Add it to sum
+# If the left child is not NULL and exceeds low Insert into queue
+# Return the resultant sum
+
+def rangeSumBST(root, low, high):
+    sum = 0
+     
+    # Base Case
+    if (root == None):
+        return 0
+
+    q = enqeue()
+    q.append(root)
+ 
+    while (len(q) > 0):
+ 
+
+        curr = q.popleft()
+        # q.pop()
+
+        if (curr.val >= low
+            and curr.val <= high):
+
+            sum += curr.val
+ 
+        if (curr.left != None
+            and curr.val > low):
+ 
+
+            q.append(curr.left)
+
+        if (curr.right != None
+            and curr.val < high):
+
+            q.append(curr.right)
+ 
+    return sum
